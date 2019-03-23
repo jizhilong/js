8fa9eb20e36c5c6de505968eaec27e01709a63a7
Ji.Zhilong <zhilongji@gmail.com>
2019-03-23 15:46:07 +0800

    implement help_cmd

--- js/commands.py
+++ js/commands.py
@@ -1,20 +1,36 @@
+import logging
+
 from .parsers import parse_command_with_records as pcr
 from .parsers import parse_command_with_text_arguments as pct
 
 cmd_processors = []
 
 
-def register_cmd(predicate=None):
-    def wrapper(f):
+class CmdRegisterItem:
+    def __init__(self, func, help_msg="", predicate=None):
+        self.func = func
+        self.help_msg = help_msg
         if predicate is None:
-            p = lambda cmd: cmd == f.__name__
+            self.predicate = lambda cmd: cmd == func.__name__
+            self.name = func.__name__
         elif isinstance(predicate, str):
-            p = lambda cmd: cmd == predicate
+            self.predicate = lambda cmd: cmd == predicate
+            self.name = predicate
         else:
-            p = predicate
-        cmd_processors.append((p, f))
-        return f
+            self.predicate = predicate
+            self.name = func.__name__
 
+    def is_triggered(self, cmd):
+        return self.predicate(cmd)
+
+    def process(self, cmd):
+        return self.func(cmd)
+
+
+def register_cmd(predicate=None, help_msg=''):
+    def wrapper(f):
+        cmd_processors.append(CmdRegisterItem(f, help_msg, predicate))
+        return f
     return wrapper
 
 
@@ -22,83 +38,100 @@ def parser(parse_func):
     """
     register arg parser to processor functions.
     """
+    import functools
 
     def wrapper(func):
-        def process(cmdline):
+        @functools.wraps(func)
+        def process_wrapped(cmdline):
             return func(*parse_func(cmdline))
-
-        return process
+        return process_wrapped
 
     return wrapper
 
 
-@register_cmd('help')
+@register_cmd('help', help_msg='显示支持的命令列表')
 @parser(pct)
-def help_cmd(cmd, arg=None):
-    return f"this is help for {arg}"
+def help_cmd(cmd_name, args: list = None):
+    if len(args) == 0:
+        commands = ', '.join(p.name for p in cmd_processors)
+        return f'支持的命令包括: {commands}'
+    else:
+        cmd_name = args[0]
+        _, processor = find_processor(cmd_name)
+        if processor is None or processor is _workout_processor:
+            return f'{cmd}: 尚不支持此命令'
+        return f'{cmd}: {processor.help_msg}'
 
 
-@register_cmd('list')
+@register_cmd('list', help_msg='显示支持的运动类型')
 @parser(pct)
-def list_workouts(cmd):
-    return f"supported workout types:"
+def list_workouts(cmd, _=None):
+    logging.info("supported workout types:")
 
 
-@register_cmd
+@register_cmd(help_msg='显示指定运动的教程')
 @parser(pct)
 def tutorial(cmd, workout):
     return f"this is tutorial for {workout}"
 
 
-@register_cmd
+@register_cmd(help_msg='显示最近运动记录')
 @parser(pct)
 def show(cmd, name=None):
     return f"recent workout records for {name}:"
 
 
-@register_cmd
+@register_cmd(help_msg='显示运动排行榜')
 @parser(pct)
 def rank(cmd):
     return f"workout ranking:"
 
 
-@register_cmd
+@register_cmd(help_msg='开启隐身模式')
 @parser(pct)
 def hideme(cmd):
     return f"I will hide you."
 
 
-@register_cmd
 @parser(pct)
 def challenge(cmd, op_or_name=None):
-    return f"challeenge: {op_or_name}."
-
-
-__registered = [predicate for predicate, _ in cmd_processors]
+    return f"challenge: {op_or_name}."
 
 
 def is_workout_name(cmd):
-    not_operation_name = not any(predicate(cmd) for predicate in __registered)
+    not_operation_name = not any(item.is_triggered(cmd) for item in cmd_processors)
     return not_operation_name
 
 
-@register_cmd(predicate=is_workout_name)
 @parser(pcr)
 def workout(name, records):
     return f"add records {records} for {name}"
 
 
+_workout_processor = CmdRegisterItem(workout)
+
+
 def find_processor(cmd_line: str):
     cmd = cmd_line.split()[0]
-    for predicate, processor in cmd_processors:
-        if predicate(cmd):
-            return processor
-    return None
+    for processor in cmd_processors:
+        if processor.is_triggered(cmd):
+            return cmd, processor
+    if is_workout_name(cmd):
+        return cmd, _workout_processor
+    return cmd, None
 
 
 def process(cmd_line: str):
-    processor = find_processor(cmd_line)
+    cmd, processor = find_processor(cmd_line)
     if processor is None:
-        return f"command {cmd_line} not supported"
-    print(f"processing with {processor.__name__} ")
-    return processor(cmd_line)
+        logging.error("command %s not supported", cmd)
+        return
+    logging.info("processing with %s", processor.name)
+    return processor.process(cmd_line)
+
+
+if __name__ == '__main__':
+    import sys
+
+    logging.basicConfig(level=logging.DEBUG)
+    print(process(' '.join(sys.argv[1:])))
